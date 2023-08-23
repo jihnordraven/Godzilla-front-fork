f7769114f5af86cda2f896783d22bf6ebd5a3258
Frank Jaskon <frankjaskon@gmail.com>
2023-08-23 19:24:30 +0300

    fix font, style lint errors, add eslint unused var rule

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,28 @@
 // временный код, нужно создать layout
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 
-import '@/app/styles/index.scss'
 import { Header } from '@/widgets/header'
 import { Sidebar } from '@/widgets/sidebar'
+
+import '@/app/styles/index.scss'
+
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className="main">
-      <Header />
-      <div className="flex-container">
-        <Sidebar />
-        <div className="content-wrapper">
-          <Component {...pageProps} />
+      <div className={inter.className}>
+        <Header />
+        <div className="flex-container">
+          <Sidebar />
+          <div className="content-wrapper">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </main>
